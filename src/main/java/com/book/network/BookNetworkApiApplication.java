f20d36ac56e3206ce25b42f28b173e9626a5e7e9
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2024-10-13 00:41:09 +0300

    create the remain entities, add auditor

--- src/main/java/com/book/network/BookNetworkApiApplication.java
+++ src/main/java/com/book/network/BookNetworkApiApplication.java
@@ -1,21 +1,15 @@
 package com.book.network;
 
-import com.book.network.entity.Role;
-import com.book.network.repository.RoleRepo;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableAsync
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
 
 }
