7f828907a34978366b9431e6c62b1f96e1cb5c28
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2024-10-18 08:40:25 +0300

    add openapi doc

---
+++ src/main/java/com/book/network/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.book.network.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Social Network API",
+                version = "1.0",
+                description = "BSN is a social network platform to exchange books between the members."
+        ),
+        servers = @Server(
+                description = "Local ENV",
+                url = "http://localhost:8088/api/v1"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi setUpGroupedOpenApi() {
+        String packages =  "com.book.network.controller";
+        return GroupedOpenApi.builder().group("BSN").packagesToScan(packages).build();
+    }
+
+}
